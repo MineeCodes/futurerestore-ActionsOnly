e63018941606f3b334aeddb7476439c80a5ef054
MineTurtlee <ghastboynguyen1534@gmail.com>
2025-02-11 12:37:33 +0700

    Update mac-build.sh

--- .github/workflows/mac-build.sh
+++ .github/workflows/mac-build.sh
@@ -5,7 +5,7 @@ export WORKFLOW_ROOT=/Users/runner/work/futurerestore-ActionsOnly/futurerestore-
 export DEP_ROOT=/Users/runner/work/futurerestore-ActionsOnly/futurerestore-ActionsOnly/dep_root
 export BASE=/Users/runner/work/futurerestore-ActionsOnly/futurerestore-ActionsOnly/
 
-cd /Users/runner/work/futurerestore/futurerestore/
+cd ${WORKFLOW_ROOT}
 ln -sf ${DEP_ROOT}/macOS_x86_64_Release/{lib/,include/} ${DEP_ROOT}/
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=$(which make) -DCMAKE_C_COMPILER=$(which clang) -DCMAKE_CXX_COMPILER=$(which clang++) -DCMAKE_MESSAGE_LOG_LEVEL="WARNING" -G "CodeBlocks - Unix Makefiles" -S ./ -B cmake-build-release-x86_64 -DARCH=x86_64 -DNO_PKGCFG=ON
 make -j4 -l4 -C cmake-build-release-x86_64
